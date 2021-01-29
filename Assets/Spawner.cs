c5e57197bbc1bf6d8a904a3dd246f6a600d42bf6
leo <medina.engineering@outlook.com>
2021-01-29 11:38:12 -0300

    Commit02

---
+++ Assets/Spawner.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject cubePrefab;
+    public GameObject spherePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Random.Range(0, 100) < 10)
+        {
+            Instantiate(cubePrefab, this.transform.position, Quaternion.identity);
+        }
+        else if (Random.Range(0, 100) < 10)
+        {
+            Instantiate(spherePrefab, this.transform.position, Quaternion.identity);
+        }
+    }
+}
